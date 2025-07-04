e9ebe083bc9d385bef4f6c4413b9e840246323eb
Sergey Knyazew <knyazew.sv@gmail.com>
2025-07-04 11:45:02 +0300

    1

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -12,37 +12,37 @@ export class BotService implements OnModuleInit {
     private readonly prisma: PrismaService,
   ) {}
 
-  async onModuleInit() {
-    // регистрируем команды
-    await this.bot.telegram.setMyCommands([
-      { command: 'menu', description: 'Открыть главное меню' },
-      { command: 'add', description: 'Добавить товар' },
-      { command: 'inventory', description: 'Показать остатки' },
-    ]);
+  async onModuleInit() {}
+  // регистрируем команды
+  // await this.bot.telegram.setMyCommands([
+  //   { command: 'menu', description: 'Открыть главное меню' },
+  //   { command: 'add', description: 'Добавить товар' },
+  //   { command: 'inventory', description: 'Показать остатки' },
+  // ]);
 
-    // подключаем middleware
-    this.bot.use(async (ctx, next) => {
-      // патчим reply через any, чтобы соответствовать оригинальному типу
-      const originalReply = (ctx as any).reply.bind(ctx);
-      (ctx as any).reply = async (text: string, extra?: any) => {
-        console.log('📤 reply →', text, extra);
-        return originalReply(text, extra);
-      };
+  // подключаем middleware
+  //   this.bot.use(async (ctx, next) => {
+  //     // патчим reply через any, чтобы соответствовать оригинальному типу
+  //     const originalReply = (ctx as any).reply.bind(ctx);
+  //     (ctx as any).reply = async (text: string, extra?: any) => {
+  //       console.log('📤 reply →', text, extra);
+  //       return originalReply(text, extra);
+  //     };
 
-      // user upsert
-      if (ctx.from?.id) {
-        const tgId = String(ctx.from.id);
-        const user = await this.prisma.user.upsert({
-          where: { telegramId: tgId },
-          update: {},
-          create: { telegramId: tgId },
-        });
-        ctx.state.user = user;
-      }
+  //     // user upsert
+  //     if (ctx.from?.id) {
+  //       const tgId = String(ctx.from.id);
+  //       const user = await this.prisma.user.upsert({
+  //         where: { telegramId: tgId },
+  //         update: {},
+  //         create: { telegramId: tgId },
+  //       });
+  //       ctx.state.user = user;
+  //     }
 
-      return next();
-    });
-  }
+  //     return next();
+  //   });
+  // }
 
   async notifyLowStock(products: Product[]) {
     if (!products.length) return;
