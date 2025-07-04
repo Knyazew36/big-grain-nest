53dc8e9649c342cb4a03b494700da40d1e8ae2f7
Sergey Knyazew <knyazew.sv@gmail.com>
2025-07-04 11:18:08 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -e
 {
 
     git pull
-    yarn prisma:migrate:deploy
-    yarn prisma:generate
+    # yarn prisma:migrate:deploy
+    # yarn prisma:generate
     yarn
     yarn build
     pm2 restart all
